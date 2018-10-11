94104bb0a0753844e8e1e23987e6a61372d3d2cd
Roman Bylbas <roman.bylbas@gmail.com>
2018-10-11 10:45:44 +0300

    Fixed compile-frontend script

--- compile-frontend.sh
+++ compile-frontend.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 cd ./resources/frontend/ \
+    && npm install \
     && npm run build \
     && cp -R ./build ../../public \
     && cp ../../public/build/index.html ../../resources/views/generated/index.blade.php \
