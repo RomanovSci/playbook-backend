a572242c06d58b5b0010bd31e5c26f0cfbb3e910
Roman Bylbas <roman.bylbas@gmail.com>
2018-10-16 13:31:03 +0300

    Installed laravel permission

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,9 +6,19 @@ use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Laravel\Passport\HasApiTokens;
 
+/**
+ * Class User
+ *
+ * @package App\Models
+ */
 class User extends Authenticatable
 {
     use Notifiable, HasApiTokens;
+
+    const ROLE_USER = 'user';
+    const ROLE_COACH = 'coach';
+    const ROLE_PLACE_ADMIN = 'playground-admin';
+    const ROLE_ADMIN = 'admin';
 
     /**
      * The attributes that are mass assignable.
