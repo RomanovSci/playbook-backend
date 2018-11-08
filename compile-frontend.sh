a8821df0f17ffd761b57e57d82a719f23ea5640b
Roman Bylbas <roman.bylbas@gmail.com>
2018-11-08 13:59:42 +0200

    Removed useless from compile-frontend

--- compile-frontend.sh
+++ compile-frontend.sh
@@ -3,5 +3,4 @@ cd ./resources/frontend/ \
     && npm install \
     && npm run build \
     && cp -R ./build ../../public \
-    && cp ../../public/build/index.html ../../resources/views/generated/index.blade.php \
-    && perl -pi -e 's/ISport/build/g' ../../resources/views/generated/index.blade.php
+    && cp ../../public/build/index.html ../../resources/views/generated/index.blade.php
