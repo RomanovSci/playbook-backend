234e43b0bee6cbc93fbca90cd9aa45cb1d149b9a
Roman Bylbas <roman.bylbas@gmail.com>
2018-11-17 12:33:19 +0200

    Added xdebug installation to ci script

--- ci_install.sh
+++ ci_install.sh
@@ -9,6 +9,8 @@ set -xe
 apt-get update -yqq
 apt-get install git -yqq
 
+pecl install xdebug && docker-php-ext-enable xdebug
+
 # Install phpunit, the tool that we will use for testing
 curl --location --output /usr/local/bin/phpunit https://phar.phpunit.de/phpunit.phar
 chmod +x /usr/local/bin/phpunit
