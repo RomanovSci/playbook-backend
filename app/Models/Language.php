e42e277a438cc9d27a3c156362c9c3df5cdc67e5
Roman Bylbas <roman.bylbas@gmail.com>
2019-01-24 16:26:06 +0200

    Added doc to language entity

--- app/Models/Language.php
+++ app/Models/Language.php
@@ -9,6 +9,35 @@ namespace App\Models;
  * @property string code
  * @property string name
  * @property string native_name
+ *
+ * @OA\Schema(
+ *      allOf={
+ *          @OA\Schema(
+ *              required={
+ *                  "code",
+ *                  "name",
+ *                  "native_name",
+ *              },
+ *              @OA\Property(
+ *                  property="id",
+ *                  type="integer",
+ *              ),
+ *              @OA\Property(
+ *                  property="code",
+ *                  type="string",
+ *              ),
+ *              @OA\Property(
+ *                  property="name",
+ *                  type="string",
+ *              ),
+ *              @OA\Property(
+ *                  property="native_name",
+ *                  type="string",
+ *              )
+ *          ),
+ *          @OA\Schema(ref="#/components/schemas/BaseModel")
+ *      }
+ * )
  */
 class Language extends BaseModel
 {
