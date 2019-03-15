71e109cd617649af66ee64554423a151653ebb85
Roman Bylbas <roman.bylbas@gmail.com>
2019-03-15 20:58:50 +0200

    hotfix

--- routes/api.php
+++ routes/api.php
@@ -77,7 +77,6 @@ Route::middleware(['role:'
     /** Booking */
     Route::prefix('booking')->group(function () {
         Route::post('/confirm/{booking}', 'API\BookingController@confirm')->name('booking.confirm');
-        Route::post('/decline/{booking}', 'API\BookingController@decline')->name('booking.decline');
         Route::get('/{bookable_type}/{uuid}', 'API\BookingController@get')->name("booking.get");
     });
 
