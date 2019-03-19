14b574689630dc52b04d540d918e242b8520531b
Roman Bylbas <roman.bylbas@gmail.com>
2019-03-19 16:54:39 +0200

    Added nexmo sms service

---
+++ app/Services/SmsDeliveryService/SmsDeliveryServiceNexmo.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Services\SmsDeliveryService;
+
+use App\Helpers\StringHelper;
+use App\Objects\Service\ExecResult;
+use Illuminate\Support\Facades\Log;
+use Nexmo\Client;
+use Nexmo\Client\Credentials\Basic;
+
+/**
+ * Class SmsDeliveryServiceNexmo
+ * @package App\Services\SmsDeliveryService
+ */
+class SmsDeliveryServiceNexmo implements SmsDeliveryServiceInterface
+{
+    const STATUS_SUCCESS = '0';
+
+    /**
+     * @var Client
+     */
+    protected $apiClient;
+
+    /**
+     * SmsDeliveryServiceMobizon constructor.
+     */
+    public function __construct()
+    {
+        try {
+            $this->apiClient = new Client(
+                new Basic(
+                    env('SMS_DELIVERY_NEXMO_API_KEY'),
+                    env('SMS_DELIVERY_NEXMO_API_SECRET')
+                )
+            );
+        } catch (\Throwable $e) {
+            Log::error($e->getMessage());
+        }
+    }
+
+    /**
+     * @inheritdoc
+     * @param string $phone
+     * @param string $text
+     * @return array
+     * @throws \Exception
+     */
+    public function send(string $phone, string $text): ExecResult
+    {
+        try {
+            $message = $this->apiClient->message()->send([
+                'to' => $phone,
+                'from' => 'PlayBook',
+                'text' => $text,
+            ]);
+        } catch (\Exception $e) {
+            Log::error($e->getMessage());
+            return ExecResult::instance()->setMessage($e->getMessage());
+        }
+
+        if ($message->getStatus() === self::STATUS_SUCCESS) {
+            return ExecResult::instance()
+                ->setSuccess()
+                ->setData(
+                    $message->getResponseData()['messages'][0]
+                );
+        }
+
+        return ExecResult::instance();
+    }
+}
