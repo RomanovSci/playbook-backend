35184b7e1a2d8cf712390bd27183d2e18d4cc848
Roman Bylbas <roman.bylbas@gmail.com>
2019-03-25 15:17:47 +0200

    Fix in TrainerInfoEditFormRequest

--- app/Http/Requests/TrainerInfo/TrainerInfoEditFormRequest.php
+++ app/Http/Requests/TrainerInfo/TrainerInfoEditFormRequest.php
@@ -2,27 +2,10 @@
 
 namespace App\Http\Requests\TrainerInfo;
 
-use App\Http\Requests\BaseFormRequest;
-
 /**
  * Class TrainerInfoEditFormRequest
  * @package App\Http\Requests\TrainerInfo
  */
-class TrainerInfoEditFormRequest extends BaseFormRequest
+class TrainerInfoEditFormRequest extends TrainerInfoCreateFormRequest
 {
-    /**
-     * @return array
-     */
-    public function rules()
-    {
-        return [
-            'playgrounds' => 'required|array',
-            'playgrounds.*' => 'required|exists:playgrounds,uuid',
-            'about' => 'string',
-            'min_price' => 'required|numeric',
-            'max_price' => 'required|numeric|gte:min_price',
-            'currency' => 'required|string|uppercase|currency',
-            'image' => 'image|max:1024',
-        ];
-    }
 }
