3dff7ebf25dcdf6031411d273fbf8b1d88965a85
Roman Bylbas <roman.bylbas@gmail.com>
2019-04-06 23:01:21 +0300

    Fixed typo

--- compile-frontend.sh
+++ compile-frontend.sh
@@ -18,7 +18,7 @@ cd ../../docker && docker exec $2 bash -c '
     rm -rf node_modules
     rm yarn.lock
     yarn install
-    export REACT_APP_API_URL=http://localhost:8001 && yarn run build
+    yarn run build
     git checkout .
     mv ./build ../../public/build
     cd ../../public/build
