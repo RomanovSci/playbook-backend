c840f642275a84b7465bb58fc33c097801cac612
Roman Bylbas <roman.bylbas@gmail.com>
2019-04-14 00:03:53 +0300

    Implemented CreateTournamentsTypes migration

--- database/migrations/2019_04_13_202737_create_tournaments_types.php
+++ database/migrations/2019_04_13_202737_create_tournaments_types.php
@@ -1,8 +1,8 @@
 <?php
 
-use Illuminate\Support\Facades\Schema;
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Support\Facades\DB;
+use Ramsey\Uuid\Uuid;
 
 class CreateTournamentsTypes extends Migration
 {
@@ -10,10 +10,25 @@ class CreateTournamentsTypes extends Migration
      * Run the migrations.
      *
      * @return void
+     * @throws Exception
      */
     public function up()
     {
-        //
+        $types = [
+            'single elimination',
+            'double elimination',
+            'round robin',
+            'swiss'
+        ];
+
+        foreach ($types as $type) {
+            DB::table('tournaments_types')->insert([
+                'uuid' => Uuid::uuid4(),
+                'name' => $type,
+                'created_at' => DB::raw('NOW()'),
+                'updated_at' => DB::raw('NOW()'),
+            ]);
+        }
     }
 
     /**
@@ -23,6 +38,6 @@ class CreateTournamentsTypes extends Migration
      */
     public function down()
     {
-        //
+        DB::table('tournaments_types')->delete();
     }
 }
