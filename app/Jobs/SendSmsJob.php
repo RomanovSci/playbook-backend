5fa7d60dbdf6cbd99a3185b89901deff4ad0fd41
Roman Bylbas <romka.bulbas@gmail.com>
2019-07-25 10:56:45 +0300

    Small fix

--- app/Jobs/SendSmsJob.php
+++ app/Jobs/SendSmsJob.php
@@ -11,7 +11,6 @@ use Illuminate\Queue\SerializesModels;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
-use Illuminate\Support\Facades\Log;
 
 /**
  * Class SendSmsJob
